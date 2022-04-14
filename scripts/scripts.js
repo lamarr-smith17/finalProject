ed864d1e6f5ef189e94e6c147c362379749ea337
LaMarr Smith <ljsmith19@my.trine.edu>
2022-04-14 15:50:57 -0400

    Added the rest of the pages Adding content to about.html currently, then doing coffee.html next

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,7 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
-const test = navLinks.querySelectorAll('li')
 
 hamburger.addEventListener('click', () =>{
     navLinks.classList.toggle('show');
 })
 
-// for(let i = 0; i < test.length; i++){
-//     test[i].addEventListener('click', ()=>{
-//         let current = document.getElementsByClassName('test');
-//         current[0].className = current[0].className.replace(' test', '');
-//         this.className += ' active';
-//     })
-// }
